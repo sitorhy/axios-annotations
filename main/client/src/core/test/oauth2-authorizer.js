fda63ecc7ca0332e49621c4d3f6968e750f0c9d7
sitorhy <sitorhy>
2022-05-20 17:18:28 +0800

    update

--- main/client/src/core/test/oauth2-authorizer.js
+++ main/client/src/core/test/oauth2-authorizer.js
@@ -3,7 +3,6 @@ import OAuth2Service from "./oauth2-service";
 
 export default class OAuth2Authorizer extends Authorizer {
     async refreshSession(session) {
-        console.log('refreshing')
         const oauthService = new OAuth2Service();
         return await oauthService.refreshToken(session);
     }
