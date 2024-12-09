c823bf4cb85a3ae2cd6d27110e8e94ad480cc6b3
sitorhy <sitorhy@gmail.com>
2024-12-10 07:42:25 +0800

    feat: 重构

---
+++ main/client/build/src/index.ts
@@ -0,0 +1,9 @@
+import B from "core";
+
+export default class A {
+    hello(){
+        console.log("hello");
+    }
+}
+
+export const classB = B;
---
+++ main/client/core/index.ts
@@ -0,0 +1,5 @@
+export default class B {
+    say() {
+        console.log('hello world');
+    }
+}
