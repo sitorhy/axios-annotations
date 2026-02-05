fcdc53ec175223caf950cd4b7f49f7830bd7f860
sitorhy <sitorhy@gmail.com>
2026-02-05 22:41:35 +0800

    feat: [3.0]调整绑定位置

--- main/client-next/core/src/lib/core/service.ts
+++ main/client-next/core/src/lib/core/service.ts
@@ -5,6 +5,19 @@ export default class Service {
     config = config;
     path = '';
 
+    constructor() {
+        // 获取被加入切面的方法
+        const methodsToBind = Object.getPrototypeOf(this).__decoratedMethods;
+
+        if (methodsToBind && Array.isArray(methodsToBind)) {
+            for (const methodName of methodsToBind) {
+                if (typeof (this as any)[methodName] === 'function') {
+                    (this as any)[methodName] = (this as any)[methodName].bind(this);
+                }
+            }
+        }
+    }
+
     async request<T = any, R = AxiosResponse<T>, D = any>(config: AxiosRequestConfig<D>) {
         return axios.request<T, R, D>(config);
     }
